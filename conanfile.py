819d68a383b05ad630a2d322ccd76ac329138ed9
Jeff Wright <jeffw387@gmail.com>
2019-01-31 19:22:43 -0800

    depend on json-shader

--- conanfile.py
+++ conanfile.py
@@ -3,15 +3,20 @@ from conans import ConanFile, CMake
 class Vkatest1Conan(ConanFile):
     name = "vkaTest1"
     version = "0.0.1"
-    license = "<Put the package license here>"
-    author = "<Put your name here> <And your email here>"
+    license = "MIT"
+    author = "Jeff Wright <jeffw387@gmail.com>"
     description = "<Description of Vkatest1 here>"
     settings = "os", "compiler", "build_type", "arch", "cppstd"
     options = {"shared": [True, False]}
     default_options = "shared=False"
     generators = "cmake", "visual_studio"
     exports_sources = "!build/*", "*"
-    requires = "tinygltf/2.0@jeffw387/testing", "filesystem/X.Y.Z@jeffw387/testing", "vkaEngine/0.0.1@jeffw387/testing", "Catch2/2.5.0@catchorg/stable"
+    requires = (
+      "tinygltf/2.0@jeffw387/testing", 
+      "filesystem/X.Y.Z@jeffw387/testing", 
+      "vkaEngine/0.0.1@jeffw387/testing", 
+      "Catch2/2.5.0@catchorg/stable",
+      "json-shader/0.0.1@jeffw387/testing")
 
     def build(self):
         cmake = CMake(self)
